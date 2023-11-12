109edf1e4e036b264e09ee9403b52a9e1ff54e3e
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-11-11 20:20:36 -0800

    ruby challenges complete

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -8,12 +8,40 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # Pseudo code:
+# create a methos that takes in a hash
+def all_us_states hash
+    # return the values in each key and used .flatten method to create one array. .sort was used to arrange array in alphbetical order.
+    hash.values.flatten.sort
+end
+
+p all_us_states us_states
+# ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Rhode Island", "Washington"]
 
 # --------------------2a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.
 
 # Pseudo code:
+# create a class called Bike
+class Bike 
+    # add getters and setters for each instance variable through an attr_accessor
+    attr_accessor :model, :wheels, :current_speed
+    # only initialize model since it doesnt nave a set value yet
+    def initialize model
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+    # create a method to return string with each instance variable included
+    def bike_info 
+        "The #{@model} bike has #{@wheels} and is going #{@current_speed}"
+    end
+end
+# set the value for model
+my_bike = Bike.new('Trek')
+
+puts my_bike.bike_info
+# The Trek bike has 2 and is going 0
 
 # -------------------2b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -23,3 +51,46 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(25) => 0
 
 # Pseudo code:
+# create a class called Bike
+class Bike 
+    # add getters and setters for each instance variable through an attr_accessor
+    attr_accessor :model, :wheels, :current_speed
+    # only initialize model since it doesnt nave a set value yet
+    def initialize model
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+    # create a method to return string with each instance variable included
+    def bike_info 
+        "The #{@model} bike has #{@wheels} and is going #{@current_speed}"
+    end
+    # creaye a method that adds the argument to the current speed
+    def speed_up speed
+        @current_speed = @current_speed + speed
+    end
+    # create a method that subtracts the argument from the current speed
+    def slow_down speed
+        @current_speed = @current_speed - speed
+        # add a conditional to never let the speed be negative
+        if @current_speed < 0
+            @current_speed = 0
+        end
+    end
+end
+# set the value for model
+my_bike = Bike.new('Trek')
+
+my_bike.speed_up(5)
+p my_bike.current_speed
+# 5
+my_bike.speed_up(10)
+p my_bike.current_speed
+# 15
+my_bike.slow_down(7)
+p my_bike.current_speed
+# 8
+my_bike.slow_down(10)
+p my_bike.current_speed
+# 0
+
